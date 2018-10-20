e4eed633ccf544703f6083c206ead1c02414ff44
Zuping <lzppro@gmail.com>
2018-10-20 01:41:06 -0500

    ...

---
+++ LeetCode/UnitTest/Solution561Test.swift
@@ -0,0 +1,18 @@
+//
+//  Solution561Test.swift
+//  LeetCodeTest
+//
+//  Created by zuping on 10/19/18.
+//  Copyright © 2018 zuping. All rights reserved.
+//
+
+import XCTest
+
+class Solution561Test: XCTestCase {
+
+    func test1() {
+        let result = Solution561().arrayPairSum([1,4,3,2])
+        assert(result == 4)
+    }
+
+}
